e2a1b50f5bd62f1df56ad61210515d8798a781ec
Buvan <bhuvanesh020405@gmail.com>
2025-06-12 00:37:45 +0530

    day 11

---
+++ Leetcode/My-LeetCode/Easy/missing number.js
@@ -0,0 +1,12 @@
+var missingNumber = function(nums) {
+       
+      let n = nums.length;
+      let totalSum = n*(n+1)/2;
+
+      let arrSum = 0;
+
+      for(let i = 0; i < n; i++){
+        arrSum = arrSum + nums[i];
+      }
+      return totalSum - arrSum;
+};
